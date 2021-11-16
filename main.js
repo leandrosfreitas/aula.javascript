bc45a8cccfedb5dfc55b1a84500038b628b7c7a6
leandrosfreitas <leandrosanfreitas@gmail.com>
2021-11-15 21:04:14 -0300

    criando script
    aula js na dio

---
+++ main.js
@@ -0,0 +1,12 @@
+var nome = "Leandro Freitas";
+var n1 = 5
+var n2 = 3
+var idade = 29;
+var frase = "Brasil é o melhor time do Mundo";
+alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade);
+console.log(n1 + n2)
+console.log(frase.replace("Brasil", "Sport"));
+alert(frase);
+alert(n1+n2)
